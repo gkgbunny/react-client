70d08a9ca205615dfbddb8b8601a1bfdcdf28919
gkgbunny <gaurav.kumar@successive.tech>
2019-02-15 16:50:00 +0530

    docs: implemented react(#40385)

---
+++ src/components/TextField/style.js
@@ -0,0 +1,16 @@
+const style = {
+  base: {
+    height: '30px',
+    width: '98.7%',
+    border: '1px solid lightgray',
+    borderRadius: '3px',
+    padding: '8px',
+  },
+  error: {
+    border: '1px solid red',
+  },
+  color: {
+    color: 'red',
+  },
+};
+export default style;
