ed7865affa7aed1295b02f0808fe88f7facce2b2
gkgbunny <gaurav.kumar@successive.tech>
2019-02-26 19:43:23 +0530

    feat: implemented material UI stylling dialog and form validation(#40835)

--- src/Trainee/Trainee.jsx
+++ src/Trainee/Trainee.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { AddDialog } from './components';
-// import { withStyles } from '@material-ui/core/styles';
 
 class Trainee extends Component {
   state = {
@@ -12,20 +11,8 @@ class Trainee extends Component {
     this.setState({ open: true });
   };
 
-  styles = theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing.unit * 2,
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  });
-
   render() {
     const { open } = this.state;
-    console.log(this.theme);
     return (
       <div>
         <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
@@ -35,8 +22,6 @@ class Trainee extends Component {
           maxWidth="xl"
           open={open}
           onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
         />
       </div>
     );
