fbafd80e83f4b40615b1022427acee72bca038e6
gkgbunny <gaurav.kumar@successive.tech>
2019-03-04 19:48:23 +0530

    feat: added sorting and onclick event on existing table(#41074)

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
+import moment from 'moment';
 import { AddDialog } from './components';
 import trainees from './data/trainee';
 import { TableComponent } from '../../components';
@@ -16,6 +16,8 @@ const styles = theme => ({
   },
 });
 
+const getFormattedDate = date => moment(date).format('dddd, MMMM Do YYYY, h:mm:ss a');
+
 const column = [
   {
     field: 'name',
@@ -25,12 +27,21 @@ const column = [
   {
     field: 'email',
     label: 'Email Address',
+    format: value => value && value.toUpperCase(),
+  },
+  {
+    field: 'createdAt',
+    label: 'Date',
+    align: 'right',
+    format: getFormattedDate,
   },
 ];
 
 class TraineeList extends Component {
   state = {
     open: false,
+    order: 'asc',
+    orderBy: '',
   };
 
   handleClickOpen = () => {
@@ -41,13 +52,26 @@ class TraineeList extends Component {
     this.setState({ open: false });
   };
 
-  traineeList = () => {
-    const { match } = this.props;
-    return trainees.map(trainee => <li><Link to={`${match.url}/${trainee.id}`}>{trainee.name}</Link></li>);
-  }
+  handleSort = (field) => {
+    // return field;
+    console.log('///////////Inside handleSort/////////////////////', field);
+
+    // const orderBy = field;
+    // let order = 'desc';
+    const { orderBy, order } = this.state;
+    if (orderBy === field && order === 'desc') {
+      order = 'asc';
+    }
+    this.setState({ order, orderBy });
+  };
+
+  handleSelect = (id) => {
+    const { match, history } = this.props;
+    history.push(`${match.url}/${id}`);
+  };
 
   render() {
-    const { open } = this.state;
+    const { open, order, orderBy } = this.state;
     const { classes } = this.props;
     return (
       <Typography className={classes.margin}>
@@ -63,14 +87,11 @@ class TraineeList extends Component {
           id="id"
           data={trainees}
           columns={column}
-          // orderBy={orderBy}
-          // order={order}
+          orderBy={orderBy}
+          order={order}
+          onSelect={this.handleSelect}
           onSort={this.handleSort}
-          onSelelect={this.handleSelect}
         />
-        <ul>
-          {this.traineeList()}
-        </ul>
       </Typography>
     );
   }
@@ -78,8 +99,10 @@ class TraineeList extends Component {
 TraineeList.propTypes = {
   classes: PropTypes.objectOf.isRequired,
   match: PropTypes.objectOf,
+  history: PropTypes.objectOf,
 };
 TraineeList.defaultProps = {
   match: {},
+  history: {},
 };
 export default withStyles(styles)(TraineeList);
