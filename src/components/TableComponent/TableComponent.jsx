38f5bdcd68a0f0bf5a5d18cc4b76c009cadcf01f
gkgbunny <gaurav.kumar@successive.tech>
2019-03-04 12:05:12 +0530

    feat: updated files as commented on PR(#41046)

--- src/components/TableComponent/TableComponent.jsx
+++ src/components/TableComponent/TableComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -19,43 +19,64 @@ const styles = theme => ({
   },
 });
 
-const TableComponent = (props) => {
-  const { classes, data, columns } = props;
-  return (
-    <>
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              {
-                columns.map(columnsItem => (
-                  <TableCell align={columnsItem.align}>
-                    {columnsItem.label || columnsItem.field}
-                  </TableCell>
-                ))
-              }
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {
-              data.map(dataItem => (
-                <TableRow key={dataItem.id}>
-                  {
-                    columns.map(item => (
-                      <TableCell align={item.align}>
-                        {dataItem[item.field]}
-                      </TableCell>
-                    ))
-                  }
-                </TableRow>
-              ))
-            }
-          </TableBody>
-        </Table>
-      </Paper>
-    </>
-  );
-};
+class TableComponent extends Component {
+  tableHead = () => {
+    const { columns } = this.props;
+    return (
+      <TableRow>
+        {
+          columns.map((columnsItem) => {
+            const { align, ...rest } = columnsItem;
+            return (
+              <TableCell align={align} {...rest}>
+                {columnsItem.label || columnsItem.field}
+              </TableCell>
+            );
+          })
+        }
+      </TableRow>
+    );
+  };
+
+  tableBody = () => {
+    const { data, columns } = this.props;
+    return (
+      data.map(dataItem => (
+        <TableRow key={dataItem.id}>
+          {
+            columns.map((item) => {
+              const { align, ...rest } = item;
+              return (
+                <TableCell align={align} {...rest}>
+                  {dataItem[item.field]}
+                </TableCell>
+              );
+            })
+          }
+        </TableRow>
+      ))
+    );
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              {this.tableHead()}
+            </TableHead>
+            <TableBody>
+              {this.tableBody()}
+            </TableBody>
+          </Table>
+        </Paper>
+      </>
+    );
+  }
+}
+
 TableComponent.propTypes = {
   classes: PropTypes.objectOf.isRequired,
   columns: PropTypes.arrayOf.isRequired,
